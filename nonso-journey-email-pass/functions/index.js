7215991b0af8b83adb2358ac63621add34af43e7
vivekgondaliya <vg13@zips.uakron.edu>
2018-07-29 15:36:39 -0700

    Cloud Functions - getJourneys - returns all the JOURNEYS

--- nonso-journey-email-pass/functions/index.js
+++ nonso-journey-email-pass/functions/index.js
@@ -139,33 +139,33 @@ exports.createStep = functions.firestore
 
 
   exports.getJourneys = functions.https.onCall((data, context) =>{
-      const journeyIds = data;
-      const uid = context.auth.uid;
+        const journeyIds = data.ids;
+        // const uid = context.auth.uid;
 
-      if(uid == null){
-        console.log("GETJOURNEYS: User not authenticated");
-        throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
-        'while authenticated.');
-      }
+        // if(uid == null){
+        //   console.log("GETJOURNEYS: User not authenticated");
+        //   throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
+        //   'while authenticated.');
+        // }
 
-      return getJourneyFromIds(journeyIds);
-        
-  });
+        return Promise.all(journeyIds.map(
+          journeyId => {
+            return ourJourneys.doc(journeyId).get().then(doc => {
+              if (!doc.exists) {
+                console.log('No such document!');
+                throw new functions.https.HttpsError('not-found', 'Journey document not found ' +
+                'Journey with id '+ journeyId +' not found');
+              } else {
+                return doc;
+              }
+            })
+        }));
+
+    });
 
 
-let getJourneyFromIds = function (ids) {
-  return Promise.all(journeyIds.map(
-    journeyId => {
-      return ourJourneys.get(journeyId).then(doc => {
-        if (!doc.exists) {
-          console.log('No such document!');
-          throw new functions.https.HttpsError('not-found', 'Journey document not found ' +
-          'Journey with id '+ journeyId +' not found');
-        } else {
-          return doc;
-        }
-      })
-  }));
-}
+  let getJourneyFromIds = function (ids) {
+
+  }
 
 //createdBy will only exits inside a journey and a step object - userId, name, imageUrl, createdType: [user, journey, step]
